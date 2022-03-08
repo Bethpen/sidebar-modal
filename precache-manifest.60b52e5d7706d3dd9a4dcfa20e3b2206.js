7fea6b97673c9c6ae2286a6f9608a832413e8b45
Bethpen <64626802+Bethpen@users.noreply.github.com>
2022-03-08 13:11:49 +0100

    Updates

---
+++ precache-manifest.60b52e5d7706d3dd9a4dcfa20e3b2206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533caaebb79e23d753880b34c5749503",
+    "url": "/sidebar-modal/index.html"
+  },
+  {
+    "revision": "01bc7a904abc712cf6bb",
+    "url": "/sidebar-modal/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "f93fba9505ce8bc47970",
+    "url": "/sidebar-modal/static/js/2.14917e05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sidebar-modal/static/js/2.14917e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01bc7a904abc712cf6bb",
+    "url": "/sidebar-modal/static/js/main.80367560.chunk.js"
+  },
+  {
+    "revision": "a8cd427f675fc95d7c22",
+    "url": "/sidebar-modal/static/js/runtime-main.328f120b.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/sidebar-modal/static/media/logo.2bb7da65.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e79a32ae0faab1303771a57bb7fe4761.js"
+  "/sidebar-modal/precache-manifest.60b52e5d7706d3dd9a4dcfa20e3b2206.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sidebar-modal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
